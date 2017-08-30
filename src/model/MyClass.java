e84a2dc1d9ab0c3fa27e072e939571e9a86673b4
jamesdrummond <help@genmymodel.com>
2017-08-30 21:02:53 +0000

    commit by jamesdrummond using genmymodel: v20170830_210250

---
+++ src/model/MyClass.java
@@ -0,0 +1,106 @@
+package model;
+
+
+/**
+* @generated
+*/
+public class MyClass {
+    
+    /**
+    * @generated
+    */
+    private String attribute;
+    
+    /**
+    * @generated
+    */
+    private Integer attribute4;
+    
+    /**
+    * @generated
+    */
+    private Integer attribute2;
+    
+    /**
+    * @generated
+    */
+    private String attribute3;
+    
+    /**
+    * @generated
+    */
+    private Boolean attribute5;
+    
+    
+    
+    /**
+    * @generated
+    */
+    public String getAttribute() {
+        return this.attribute;
+    }
+    
+    /**
+    * @generated
+    */
+    public String setAttribute(String attribute) {
+        this.attribute = attribute;
+    }
+    
+    /**
+    * @generated
+    */
+    public Integer getAttribute4() {
+        return this.attribute4;
+    }
+    
+    /**
+    * @generated
+    */
+    public Integer setAttribute4(Integer attribute4) {
+        this.attribute4 = attribute4;
+    }
+    
+    /**
+    * @generated
+    */
+    public Integer getAttribute2() {
+        return this.attribute2;
+    }
+    
+    /**
+    * @generated
+    */
+    public Integer setAttribute2(Integer attribute2) {
+        this.attribute2 = attribute2;
+    }
+    
+    /**
+    * @generated
+    */
+    public String getAttribute3() {
+        return this.attribute3;
+    }
+    
+    /**
+    * @generated
+    */
+    public String setAttribute3(String attribute3) {
+        this.attribute3 = attribute3;
+    }
+    
+    /**
+    * @generated
+    */
+    public Boolean getAttribute5() {
+        return this.attribute5;
+    }
+    
+    /**
+    * @generated
+    */
+    public Boolean setAttribute5(Boolean attribute5) {
+        this.attribute5 = attribute5;
+    }
+    
+}
